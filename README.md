1c98d38d237d3cb0fab43ed9f744a3099296b389
Reiq.Laptop <reiqvideos@gmail.com>
2021-07-26 06:00:48 -0400

    Los inputs se crean de una forma más dinámica con javascript

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Sitio web dedicado a la venta de comida para todo tipo de celebración
 
 ---
 
-## Enlace para ver el estado actual
+## Enlace para ver el estado actual (NO ESTÁ TERMINADA)
 ### [Sitio web de Sarai Bufet](https://hernanreiq.github.io/sarai-bufet/) :heart:
 
 ---
--- js/script.js
+++ js/script.js
@@ -1,29 +1,61 @@
-const modal_body = document.getElementById('modal-body');
-
 /* OBTENIENDO EL MENU QUE ESTA EN EL JSON */
+var menu;
 function ObtenerMenuJSON(){
     axios({
         method: 'GET',
         url: 'json/menu.json'
     }).then (res => {
-        const menu = res.data;
+        menu = res.data;
+        //CUANDO SE OBTENGA EL MENÚ, SE CREARÁ EL PRIMER INPUT
+        masBandejas();
     }).catch(err => {
         console.log('Hay un error con el menu.json\n', err);
     });
 }
-
 ObtenerMenuJSON();
 
+var inputs_borrados = [];
+var cantidad_input = 0;
+
 function borrarOpcion(opcion){
+    inputs_borrados.push(opcion);
     document.getElementById(opcion).parentElement.replaceWith('');
-    console.log('ID:', opcion, 'fue borrado!');
 }
 
-function masBandejas(){
-    document.getElementById('contenedor-inputs').innerHTML += `
-        Hola    <br>
-    `;
-}
+/* 
+    EN ESTE BLOQUE DE CODIGO SE VAN A CREAR LOS INPUTS PARA LAS OPCIONES O BANDEJAS
+*/
 var mas_bandejas = document.getElementById('mas-bandejas');
-
 mas_bandejas.addEventListener('click', masBandejas);
+
+function masBandejas(){ 
+    if (inputs_borrados.length > 0){
+        var id_input = inputs_borrados[0];
+        inputs_borrados.shift();
+    } else {
+        cantidad_input += 1;
+        var id_input = 'inputGroup-' + cantidad_input;
+    }
+    creadorInputs(id_input);
+}
+
+function creadorInputs(id_input){
+    document.getElementById('contenedor-inputs').innerHTML += `
+    <div class="input-group my-3">
+        <div class="input-group-prepend">
+            <label class="input-group-text font-weight-bold" for="${id_input}">Opciones</label>
+        </div>
+        <select class="custom-select" id="${id_input}">
+            <option selected>Elegir bandeja</option>
+        </select>
+        <button class="btn btn-danger border" onclick="borrarOpcion('${id_input}')">
+            <i class="far fa-trash-alt"></i>
+        </button>
+    </div>
+    `;
+    for (var i = 0; i < menu.length; i++){
+        document.getElementById(`${id_input}`).innerHTML += `
+            <option value="${i}">${menu[i].nombre}</option>
+        `;
+    }
+}
